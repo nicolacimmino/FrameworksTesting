8e34aa1d8439bfdf18446d7ed313f2a2980e6702
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-09-16 11:50:36 +0200

    Base app for accounts service

---
+++ src/services/accounts/src/main/kotlin/com/cimminonicola/finanaceplanneraccounts/FinanaceplannerAccountsApplication.kt
@@ -0,0 +1,12 @@
+package com.cimminonicola.finanaceplanneraccounts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanaceplannerAccountsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanaceplannerAccountsApplication>(*args)
+	println("test")
+}
