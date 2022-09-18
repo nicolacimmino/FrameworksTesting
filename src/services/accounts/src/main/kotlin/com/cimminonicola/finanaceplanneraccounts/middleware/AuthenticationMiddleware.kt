814c467abdab71d5b6186753161b3ea3fc2237a8
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-09-18 17:53:06 +0200

    Add middleware, wip

--- src/services/accounts/src/main/kotlin/com/cimminonicola/finanaceplanneraccounts/middleware/AuthenticationMiddleware.kt
+++ src/services/accounts/src/main/kotlin/com/cimminonicola/finanaceplanneraccounts/middleware/AuthenticationMiddleware.kt
@@ -1,8 +1,6 @@
 package com.cimminonicola.finanaceplanneraccounts.middleware
 
-import com.cimminonicola.finanaceplanneraccounts.errors.InputInvalidApiException
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
