79d130770be1c13ca175cb95ec74115214a411de
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-10-03 22:05:44 +0200

    Fix tests

--- src/services/accounts/src/test/kotlin/com/cimminonicola/finanaceplanneraccounts/TokensApiIntegrationTestsApi.kt
+++ src/services/accounts/src/test/kotlin/com/cimminonicola/finanaceplanneraccounts/TokensApiIntegrationTestsApi.kt
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
