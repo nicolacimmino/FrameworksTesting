5eae16b5ef569b68651b88be885c03f4cfe26e02
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-11-01 13:40:56 +0100

    Add flutter app

--- src/client/finance_planner/test/widget_test.dart
+++ src/client/finance_planner/test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,17 +17,17 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FinancePlannerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // // Verify that our counter starts at 0.
+    // expect(find.text('0'), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
+    //
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    //sleep(Duration(seconds: 10));
 
-
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('savings5'), findsOneWidget);
   });
 }
