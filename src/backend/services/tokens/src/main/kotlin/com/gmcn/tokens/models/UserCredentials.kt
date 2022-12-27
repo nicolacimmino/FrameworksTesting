955a08dc168605e81e36a618058152376684484f
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-12-27 13:00:38 +0100

    WIP

--- src/backend/services/tokens/src/main/kotlin/com/gmcn/tokens/models/UserCredentials.kt
+++ src/backend/services/tokens/src/main/kotlin/com/gmcn/tokens/models/UserCredentials.kt
@@ -1,18 +1,24 @@
 package com.gmcn.tokens.models
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
+// Note: @Document is mongo specific as the JPA @Table annotation is not honoured by MongoRepository
+
+@Entity
+@Table(name = "user-credentials")
 @Document(collection = "user-credentials")
 class UserCredentials {
-    @Column
+    @Id
+    @Column(name = "userId", nullable = false)
     var userId: String = ""
 
-    @Column
     var email: String = ""
 
-    @Column
     var password: String = ""
         set(value) {
             field = BCryptPasswordEncoder().encode(value)
