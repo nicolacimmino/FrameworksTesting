25f9f9019a31023b380bfc3aa7e8d077724a9fcd
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-12-29 16:11:39 +0100

    Typos

--- src/backend/service-registry/src/main/kotlin/com/gmnc/serviceregistry/ServerviceRegistryApplication.kt
+++ src/backend/service-registry/src/main/kotlin/com/gmnc/serviceregistry/ServerviceRegistryApplication.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-// see: https://medium.com/@ocrnshn/microservices-with-spring-c30dfef8782b
-
 @SpringBootApplication
 @EnableEurekaServer
 class ServerviceRegistryApplication
